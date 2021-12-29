428b18a4eaed975560454e57544df6fb2f50c7d2
luisanch <luissanchezmtn@gmail.com>
2021-12-29 20:30:17 +0100

    Real Source Plot

--- Init.m
+++ Init.m
@@ -2,61 +2,72 @@ clear variables
 close all
 clc
 
-h = 200;
-source = [0 -100];
-hr = 0 : -10 : -h;
-receivers = [(zeros(size(hr)) + 100)', hr'];
-n_s = 10; %amount of computed sources
-totalTime = 1.51; % in s
-sampleTime = 0.01;
-a = 10;
-freq = 1; %in hz
-c = 1500;
-s_t = emit(totalTime, sampleTime, @sin, freq, a);
-n_gg_t= fitArrays(getMultipleRecordings(source, h, n_s, receivers, sampleTime, s_t, c));
-sr_t = getBackPropagation(n_gg_t, receivers, source, sampleTime, n_s, c, h);
-disp(size(sr_t))
-plot(sr_t(:,1), sr_t(:,2))
-figure()
-plot(s_t(:,1), s_t(:,2))
-figure()
-plotAll(n_gg_t)
+load('received_signals.mat')
 
-yRange = -(0 : 10 : 100);
-xRange = (0 : 10 : 100);
+%% inputs
+h = 150; %distance to ocean floor fromthe surface
+hr = 15 : 15 : h - 15; %z positions of the transceivers
+receivers = [(zeros(size(hr)) + 1100)', hr'];  %array with x,z positions of the transceivers
+n_s = 8; %amount of computed sources
+sampleTime = 8.3361e-04; 
+c = 1500; %speed of sound
+resolution = 10; %resolution in meters for back prpagation in selected area
+yRange = (0 : resolution : h);  %vertical range of selected area
+xRange = (0 : resolution : 200);  %horizontal range of selected area
 
-% test{1} = record(s_t, source, receivers(1,:), c, sampleTime, 1, h);
-% test{2} = record(s_t, source, [100 -100], c, sampleTime, 2, h);
-% t3 = fitArrays(test);
-% plotAll(t3)
+%% inputs for simulated signal 
+source = [50 50]; %source loccation
+a = 10; %source signal amplitude
+freq = 10; %in hz 
+totalTime = 1.6; % in s
+
+%% Simulate Data
+%s_t = emit(totalTime, sampleTime, @mySinc, freq, a);
+%n_gg_t= fitArrays(getMultipleRecordings(source, h, n_s, receivers, sampleTime, s_t, c)); 
+%sr_t = backPropagate(n_gg_t, receivers, source, sampleTime, n_s, c, h);
+
+%% Plots for simulated data
+% plot(s_t(:,1), s_t(:,2))
+% title("original")
+% figure()
+% plot(sr_t(:,1), sr_t(:,2))
+% title("back Propagated") 
+
+%% Get Real Data 
+n_gg_t= greenToRecorded(green, sampleTime);
+
+%% Plots 
+figure()
+plotAll(n_gg_t, false)
+title("recorded")
 grid = getMultipleBackPropagation(n_gg_t, receivers, sampleTime, n_s, c, h, yRange, xRange);
 figure()
-image(grid,'CDataMapping','scaled')
+image(grid,'CDataMapping','scaled') 
+xticklabels((xticks*resolution)-resolution+xRange(1)) 
+yticklabels((yticks*resolution)-resolution+yRange(1))
 colorbar
 
 %% back propagate in multiple points
 function grid = getMultipleBackPropagation(n_gg_t, receivers, sampleTime, n_s, c, h, yRange, xRange)
- grid = zeros([size(xRange,2) size(yRange,2)]);
- disp(grid)
+grid = zeros([size(yRange,2) size(xRange,2)]);
 for x = 1 : 1 : size(xRange,2)
     for y = 1 : 1 : size(yRange,2)
-        sr_t = getBackPropagation(n_gg_t, receivers, [xRange(x) yRange(y)], sampleTime, n_s, c, h);
-        grid(x,y) = sum(sr_t(:,2));
+        sr_t = backPropagate(n_gg_t, receivers, [xRange(x) yRange(y)], sampleTime, n_s, c, h);
+        grid(y,x) = max(sr_t(:,2));
     end
 end
 end
 
 %% back propagation
-function sr_t = getBackPropagation(n_gg_t, receivers, atLocation, sampleTime, n_s, c, h)
-n_ss_t = {}; 
+function sr_t = backPropagate(n_gg_t, receivers, atLocation, sampleTime, n_s, c, h)
+n_ss_t = {};
 for i =  1 : 1 : size(n_gg_t, 2)
     reverse_source = receivers(i,:);
-    reverse_h = h - receivers(i,2);
     reverse_receiver = atLocation;
     reverse_s_t = [n_gg_t{i}(:,1), flipud(n_gg_t{i}(:,2))];
-    n_ss_t{i} = sumArrays(fitArrays(recordMultSources(reverse_source, reverse_h, n_s, reverse_receiver, sampleTime, reverse_s_t, c)));
+    n_ss_t{i} = sumArrays(fitArrays(recordMultSources(reverse_source, h, n_s, reverse_receiver, sampleTime, reverse_s_t, c)));
 end
-sr_t =  sumArrays(fitArrays(n_ss_t));
+sr_t = normalize(sumArrays(fitArrays(n_ss_t))); %not sure about this
 end
 
 %% record signal utils
@@ -70,7 +81,7 @@ end
 
 function gg_t = recordMultSources(source, h, n_s, receiver, sampleTime, s_t, c)
 for j = 1 : 1 : n_s
-    gg_t{j} = record(s_t, source, receiver, c, sampleTime, j, h);
+    gg_t{j} = record(s_t, source, receiver, c, sampleTime, j-1, h);
 end
 end
 
@@ -103,25 +114,54 @@ else         % j is odd
 end
 end
 
-function e = getEpsilon(j) 
-if ~mod(j,2) % j is even
-    e = -1^(j/2);
-else         % j is odd
-    e = -1^((j-1)/2);
-end
+function e = getEpsilon(j)
+e = 1;
+%%commented this out for the time being since e = +1
+% if ~mod(j,2) % j is even
+%     e = (-1)^(j/2);
+% else         % j is odd
+%     e = (-1)^((j-1)/2);
+% end
 end
 
 %% function generation utils
-function s_t = emit(totalTime, sampleTime, func, freq, a)
-freqRad = 2 * pi * freq;
-sampleSpace = 0 : sampleTime : totalTime;
-s_t = zeros(totalTime / sampleTime, 2);
+function n_gg_t = greenToRecorded(green, sampleTime)
+n_gg_t = {};
+for i = 1 : 1 : size(green,1)
+    n_gg_t{i} = [(0:1:size(green,2)-1)',green(i,:)'];
+end
+end
 
-for i = 1 : 1 : totalTime / sampleTime
-    s_t(i, 1) = sampleSpace(i);
+function sinc = mySinc(t)
+if t == 0
+    sinc = 1;
+else
+    sinc = sin(t)/(t);
+end
+end
+
+function s_t = emit(totalTime, sampleTime, func, freq, a)
+freqRad = pi * freq; %half
+sampleSpace = 0 : sampleTime : totalTime/2;
+sampleSpace = [flip(-sampleSpace) sampleSpace];
+s_t = zeros(size(sampleSpace,2), 2);
+
+for i = 1 : 1 : size(sampleSpace,2)
+    s_t(i, 1) =  sampleSpace(i) + sampleSpace(size(sampleSpace,2));
     s_t(i, 2) = propagateWave(func,freqRad, sampleSpace(i), a);
 end
 end
+
+% function s_t = emit(totalTime, sampleTime, func, freq, a)
+% freqRad = 2 * pi * freq;
+% sampleSpace = 0 : sampleTime : totalTime;
+% s_t = zeros(totalTime / sampleTime, 2);
+%
+% for i = 1 : 1 : totalTime / sampleTime
+%     s_t(i, 1) = sampleSpace(i);
+%     s_t(i, 2) = propagateWave(func,freqRad, sampleSpace(i), a);
+% end
+% end
 
 function y = propagateWave(func,freq, t, a)
 y = a * func(freq * t);
@@ -152,9 +192,20 @@ end
 end
 
 %% plot Utils
-function y = plotAll(cellArray)
-for i = 1 : 1 : size(cellArray,2)
+function y = plotAll(cellArray, singleWindow)
+legendLabel = {}; 
+for i = 1 : 1 : size(cellArray,2) 
+    if singleWindow
+    legendLabel{i} = num2str(i);
     plot(cellArray{i}(:,1), cellArray{i}(:,2));
     hold on
+    else
+    subplot(size(cellArray,2), 1, i)
+    plot(cellArray{i}(:,1), cellArray{i}(:,2));
+    legend({num2str(i)},'Location','southwest') 
+    end
+end
+if singleWindow
+legend(legendLabel,'Location','southwest')
 end
 end
